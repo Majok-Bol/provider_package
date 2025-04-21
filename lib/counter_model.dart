f37194d3e03c991041a95a94c8a3caf91c4800a6
Majok-Bol <bolmajok001@gmail.com>
2025-04-21 19:01:40 +0300

    added add and delete buttons

--- lib/counter_model.dart
+++ lib/counter_model.dart
@@ -1,2 +1,30 @@
 //handle counter
-class Counter
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+//this class will use change notifier mixin to
+//alert listeners of changes
+//in its state
+class Counter with ChangeNotifier{
+  //keep track of counts
+  int _count = 0;
+//getter for the count
+//expose private variable to outside
+//allows other widgets to get current value of the count
+//but cannot modify it directly
+//because it is a getter
+int get count=>_count;
+
+void increment(){
+  _count++;
+  //alert listeners of changes
+  //rebuild the UI
+  notifyListeners();
+}
+//reset
+void reset(){
+  _count=0;
+  notifyListeners();
+}
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_package/counter_model.dart';
 void main(){
-  runApp(MyApp());
+  //acts as a bridge between myApp and counter class
+  //allows widgets access counter class
+  //and listens to its changes
+  //creates instance of counter class
+  //so that widgets can
+  runApp(ChangeNotifierProvider(create:(_)=>Counter(),
+    child: const MyApp(),
+  ));
 }
 class MyApp extends StatelessWidget{
   const MyApp({super.key});
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      home:Scaffold(
-        appBar: AppBar(title: Text('Provider package'),centerTitle: true,backgroundColor: Colors.blue.shade200,),
+      debugShowCheckedModeBanner: false,
+      home:HomePage(),
 
-      ),
     );
   }
+}
+class HomePage extends StatefulWidget{
+  @override
+  HomePageState createState()=>HomePageState();
+}
+class HomePageState extends State<HomePage>{
+  @override
+  Widget build(BuildContext context){
+    //create instance of counter model
+    var counter=Provider.of<Counter>(context);
+    return Scaffold(appBar: AppBar(title: Text('Provider package'),centerTitle: true,backgroundColor: Colors.blue.shade200,),
+    body:Container(
+      padding: EdgeInsets.all(10),
+
+      child: Column(
+      children: [
+        Row(children: [
+          ElevatedButton(onPressed:() {
+            counter.increment();
+
+          },style:ElevatedButton.styleFrom(backgroundColor: Colors.green), child:Icon(Icons.add)),
+          SizedBox(width: 30,),
+          ElevatedButton(onPressed:() {
+            counter.reset();
+
+          },style:ElevatedButton.styleFrom(backgroundColor: Colors.red),child:Icon(Icons.delete))
+        ],),
+        Center(child: Consumer<Counter>(builder:(context,counter,child){
+          return Text('Count: ${counter.count}');
+
+        }),)
+
+      ],
+    ),)
+    );
+  }
+
 }
